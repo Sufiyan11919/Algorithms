fd2d8946e6d58d6a260d7a7f48079dd9bcb78a50
sufiyan11919 <sufiyanshaikh11919@gmail.com>
2024-02-12 19:31:51 -0800

    2108 leetcode by recursion

---
+++ CS545_12th_Feb/first_palindromic_string.java
@@ -0,0 +1,20 @@
+package CS545_12th_Feb;
+
+class first_palindrome_string {
+    public String firstPalindrome(String[] words) {
+        for (int i = 0; i < words.length; i++) {
+            if (isPalindrome(words[i])) {
+                return words[i];
+            }
+        }
+        return "";
+    }
+    public static boolean isPalindrome(String word) {
+        if (word.length() <= 1) {
+            return true;
+        }else if (word.charAt(0) != word.charAt(word.length() - 1)) {
+            return false;
+        }
+        return isPalindrome(word.substring(1, word.length() - 1));
+    }
+}
