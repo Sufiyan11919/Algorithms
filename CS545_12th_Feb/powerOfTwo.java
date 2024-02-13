861e803a4ee828449d10ec77039072e38c825f62
sufiyan11919 <sufiyanshaikh11919@gmail.com>
2024-02-12 22:34:37 -0800

    power of two recursion

---
+++ CS545_12th_Feb/powerOfTwo.java
@@ -0,0 +1,16 @@
+class isPowerOfTwo {
+    public static boolean isPowOfTwo(int n) {
+        if (n==1) {
+            return true;
+        }
+        if (n ==0) {
+            return false;
+        }
+        boolean smallerAnswer = isPowOfTwo(n/2);
+        return n % 2 ==0 && smallerAnswer;
+    }
+    public static void main(String[] args) {
+        int n = 16;
+        System.out.println(isPowOfTwo(n));
+    }
+}
--- CS545_12th_Feb/sq_sort_array.java
+++
@@ -1,9 +0,0 @@
-package CS545_12th_Feb;
-// Input: nums = [-4,-1,0,3,10]
-// Output: [0,1,9,16,100]
-public class sq_sort_array {
-    public int[] sortedSq(int[] nums){
-        int positiveIndex = 0;
-        return nums;
-    }
-}
