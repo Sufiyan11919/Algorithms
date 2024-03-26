a15dafe77718a2441b751238ab7f71692961db0a
sufiyan11919 <sufiyanshaikh11919@gmail.com>
2024-03-25 20:14:26 -0700

    Preorder trversal

--- CS545_25th_March/preorder.java
+++ CS545_25th_March/preorder.java
@@ -1,5 +1,38 @@
 package CS545_25th_March;
 
-public class preorder {
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<>();
+        preorderTraversalRecursive(root, res);
+        return res;
+    }
+
+    private void preorderTraversalRecursive(TreeNode node, LinkedList<Integer> res) {
+        if (node == null)
+            return;
+
+        res.add(node.val);
+        preorderTraversalRecursive(node.left, res);
+        preorderTraversalRecursive(node.right, res);
+    }
+        
     
 }
