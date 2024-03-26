b4c6c16c89b67526a28ba44638cbef5ab7bbeaf6
sufiyan11919 <sufiyanshaikh11919@gmail.com>
2024-03-25 19:55:46 -0700

    added folder

--- CS545_26th_Feb/KthSmallest.java
+++ CS545_26th_Feb/KthSmallest.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class KthSmallest {
+public class KthSmallest {
+    public int kthSmallest(int[][] matrix, int k) {
+        int l = matrix[0][0];
+        int r = matrix[matrix.length - 1][matrix.length - 1];
+        while(l < r) {
+            int mid = (l + r) / 2;
+            int count = 0;
+            int j = matrix.length - 1;
+
+            for(int i = 0; i < matrix.length; i++) {
+                while (j >= 0 && matrix[i][j] > mid) {
+                    j--;
+                }
+                count = count (j + 1);
+            }
+            if(count < k) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return l;
+    }
 }
