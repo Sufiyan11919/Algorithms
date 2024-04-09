95ee56812b6d09a008662e79c61a63c3f528e8de
sufiyan11919 <sufiyanshaikh11919@gmail.com>
2024-04-08 19:59:25 -0700

    second leetcode problem, max product

---
+++ CS545_8th_April/max_product.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        heapSort(nums);
+        int x = nums[nums.length - 1];
+        int y = nums[nums.length - 2];
+        return (x - 1) * (y - 1);
+    }
+    
+    public void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+
+    public void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+}
